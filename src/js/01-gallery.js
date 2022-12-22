2005735fc576b6641d31c839e0360ebb2a73dfe5
Transcendence <baydindenys@ukr.net>
2022-12-22 14:23:00 +0200

    task 2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,8 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+
+// SimpleLightbox import
 import SimpleLightbox from "simplelightbox";
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
